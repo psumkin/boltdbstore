7d597d217e2b18677e45d5208c046662a8bc7c2e
Pavlo Sumkin <psumkin@mirantis.com>
2016-08-29 17:38:41 +0300

    Import code from Mirantis/northshore

---
+++ store.go
@@ -0,0 +1,138 @@
+// Copyright 2016 The NorthShore Authors All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package boltdbstore
+
+import (
+	"encoding/json"
+	"errors"
+
+	log "github.com/Sirupsen/logrus"
+	"github.com/spf13/viper"
+
+	"github.com/boltdb/bolt"
+)
+
+// Storable defines collection interface
+type Storable interface {
+	// Bucket returns the bucket name
+	Bucket() []byte
+	// Next is an iterator, takes key returns ref to item instance
+	Next([]byte) interface{}
+}
+
+// GetStorable loads all items from boltdb Bucket
+func GetStorable(items Storable) error {
+	bucket := items.Bucket()
+	db := openDBBucket(bucket)
+	defer db.Close()
+
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(bucket)
+
+		b.ForEach(func(k, v []byte) error {
+
+			if err := json.Unmarshal(v, items.Next(k)); err != nil {
+				log.Errorln("#DB,#GetStorable", err)
+				return err
+			}
+
+			return nil
+		})
+
+		return nil
+	})
+	return err
+}
+
+// Delete deletes key from boltdb Bucket
+func Delete(bucket []byte, key []byte) error {
+	db := openDBBucket(bucket)
+	defer db.Close()
+
+	if err := db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket(bucket).Delete(key)
+	}); err != nil {
+		log.Errorln("#DB,#Delete", err)
+		return err
+	}
+	return nil
+}
+
+// Load loads item from boltdb Bucket
+func Load(bucket []byte, key []byte, v interface{}) error {
+	db := openDBBucket(bucket)
+	defer db.Close()
+
+	log.Debugln("#DB", "Load data from DB")
+	err := db.View(func(tx *bolt.Tx) error {
+		b := tx.Bucket(bucket)
+		buf := b.Get(key)
+		if buf == nil {
+			log.Debugln("#DB,#Load,#Nil")
+			return errors.New("Key does not exist or key is a nested bucket")
+		}
+
+		if err := json.Unmarshal(buf, &v); err != nil {
+			log.Errorln("#DB,#Load", err)
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+// Save stores item in boltdb Bucket as JSON
+func Save(bucket []byte, key []byte, v interface{}) error {
+	db := openDBBucket(bucket)
+	defer db.Close()
+
+	log.Debugln("#DB", "Save data to DB")
+	err := db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket(bucket)
+		vEncoded, err := json.Marshal(v)
+		log.Debugln("#DB,#Save,#Encoded", string(vEncoded))
+		if err != nil {
+			log.Errorln("#DB,#Store", err)
+			return err
+		}
+		if err := b.Put(key, vEncoded); err != nil {
+			log.Errorln("#DB,#Store", err)
+			return err
+		}
+		return nil
+	})
+	return err
+}
+
+func openDBBucket(bucket []byte) *bolt.DB {
+	path := viper.GetString("BoltDBPath")
+	log.Debugln("Open #DB -> ", path)
+	db, err := bolt.Open(path, 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Update(func(tx *bolt.Tx) error {
+		_, err = tx.CreateBucketIfNotExists(bucket)
+		if err != nil {
+			return err
+		}
+		return nil
+	}); err != nil {
+		log.Fatal(err)
+	}
+
+	return db
+}
